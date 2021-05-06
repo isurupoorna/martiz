c9df3dc08c8499608319d5bdb55e761379e4e1e5
RlShan <69212663+RlShan@users.noreply.github.com>
2021-05-06 18:35:23 +0530

    create customer components

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { CustomersComponent } from './customers.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductsComponent } from './products/products.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'sign', component: SigninComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'productdetails', component: ProductDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
-  declarations: [CustomersComponent],
+  declarations: [CustomersComponent, HomeComponent, ProductsComponent, LoginComponent, SigninComponent, CartComponent, HeaderComponent, FooterComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
