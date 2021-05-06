5ccb46590dfc42a3918498d693f5e46832b2967c
isurupoorna <isuru.poorna28@gmail.com>
2021-05-06 18:19:29 +0530

    manage users
    show users and enable edit

--- src/app/admin/service/user.service.ts
+++ src/app/admin/service/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(public fireservices:AngularFirestore) { }
+
+
+  get_allUsers(){
+    return this.fireservices.collection('user').snapshotChanges();
+  }
+
+
 }
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../service/user.service";
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+
+  user : any;
+  id : string;
+  userId : number;
+  userName : string;
+  userAddress : string;
+  userPhone : string;
+  status: number;
+  
+  isEdit: boolean = false;
+
+  constructor(public userService:UserService) { }
 
   ngOnInit(): void {
+    this.userService.get_allUsers().subscribe(data =>{
+      this.user = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          userId: e.payload.doc.data()['userId'],
+          userName: e.payload.doc.data()['userName'],
+          userAddress: e.payload.doc.data()['userAddress'],
+          userPhone: e.payload.doc.data()['userPhone'],
+          status: e.payload.doc.data()['status'],
+        };
+
+      })
+      console.log(this.user);
+      console.log(this.user.id);
+      
+    })
+  }
+
+  editUser(){
+    this.isEdit = true;
+  }
+
+  cancel(){
+    this.isEdit = false;
   }
 
 }
