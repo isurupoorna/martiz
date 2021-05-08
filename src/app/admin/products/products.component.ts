590d81771f35b04c4692807389df5f4eeed75c89
isurupoorna <isuru.poorna28@gmail.com>
2021-05-09 00:04:53 +0530

    complete admin add image
    product image add

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../service/product.service';
-
+import { AngularFireStorage } from '@angular/fire/storage';
+import {Observable} from 'rxjs';
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -19,19 +21,25 @@ export class ProductsComponent implements OnInit {
   description:string;
   message:string;
   tagName:string;
+  url:string;
 
   isedit:boolean = false;
 
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  profileUrl: Observable<string | null>;
+
   constructor(public productservice:ProductService , 
               private router: Router,
-              private rought: ActivatedRoute) { }
+              private rought: ActivatedRoute,
+              private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
     this.productservice.get_allProduct().subscribe(data =>{
       this.product = data.map(e => {
         return {
           id: e.payload.doc.id,
-          sku: e.payload.doc.data()['sku'],
+          sku: e.payload.doc.data()['sku'], 
           Productname: e.payload.doc.data()['name'],
           price: e.payload.doc.data()['price'],
           category : e.payload.doc.data()['category'],
@@ -44,6 +52,7 @@ export class ProductsComponent implements OnInit {
   }
 
   createItem(){
+    console.log(this.url);
 
     let tags = [];
     this.tagName = this.name;
@@ -62,6 +71,7 @@ export class ProductsComponent implements OnInit {
     Record['category'] = this.category;
     Record['description'] = this.description;
     Record['tags'] = tags;
+    Record['url'] = this.url;
 
 
     this.productservice.create_NewItem(Record).then(res => {
@@ -93,11 +103,6 @@ export class ProductsComponent implements OnInit {
     console.log(this.price);
     console.log(this.Productname);
     console.log(this.category);
-    
-    // Record.editSku = Record.sku;
-    // Record.editprice = Record.price;
-    // Record.editcategory = Record.category;
-    // Record.editdescription = Record.description;
 
   }
 
@@ -129,6 +134,28 @@ export class ProductsComponent implements OnInit {
 
   deleteProduct(product_id){
     this.productservice.deleteProduct(product_id);
+  }
+
+  chooseFile(event){
+    const file = event.target.files[0];
+    const filePath = 'ona ekak';
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        fileRef.getDownloadURL().subscribe((downloadURL) => {
+          
+          
+          this.url = downloadURL;
+          console.log(this.url);
+          //fileUpload.name = fileUpload.file.name;
+         // this.saveFileData(fileUpload);
+        });
+      })
+    ).subscribe();
+    
   }
 
 }
